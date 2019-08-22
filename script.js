ff353ed25bd1c71b9293ba0677a4e5cbaf2ebf07
tori66 <53177359+tori66@users.noreply.github.com>
2019-08-23 02:49:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" activedot", "");
     }
     slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    dots[slideIndex-1].className += " activedot";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 }
